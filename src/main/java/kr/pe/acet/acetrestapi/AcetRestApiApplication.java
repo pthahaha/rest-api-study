74ad8b06a858f60d305503dc7edd9c54895d4075
teri.epi <teri.epi@kakaoenterprise.com>
2022-02-24 15:51:30 +0900

    project created

---
+++ src/main/java/kr/pe/acet/acetrestapi/AcetRestApiApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.acet.acetrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcetRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcetRestApiApplication.class, args);
+    }
+
+}
